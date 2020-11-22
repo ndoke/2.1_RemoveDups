0996cdc79b20482a68f4e726a389161afa5859d6
ndoke <ndoke@cisco.com>
2020-11-22 20:29:46 +0530

    Adds Java doc to the existing class.

--- src/main/java/com/training/LinkedListNode.java
+++ src/main/java/com/training/LinkedListNode.java
@@ -1,5 +1,13 @@
 package com.training;
 
+/**
+ * {@link LinkedListNode} has basic methods for a LinkedListNodeLinked list.
+ *
+ * {@link LinkedListNode#findLength()} returns length of the Linked list.
+ *
+ * @author Nachiket Doke
+ * @since 1.0
+ */
 public class LinkedListNode {
     public int data;
     public LinkedListNode next;
@@ -9,6 +17,11 @@ public class LinkedListNode {
         this.next = null;
     }
 
+    /**
+     * Returns length of a linked list
+     *
+     * @return length of the linked list
+     */
     public int findLength() {
         int length = 0;
         LinkedListNode current = this;
